431168412e2edbf05f12159056199985ab22d65d
Taras Nakonechnyi <Taras.Nakonechnyi@orckestra.com>
2022-09-07 07:54:00 +0000

    Merged PR 10661: [ContentPersonalization] evaluate data for only used providers in query
    [ContentPersonalization] evaluate data for only used providers in query
    
    Related work items: #57223

---
+++ ConditionalContent.Tests/Services/ConditionalContentServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+using Orckestra.ExperienceManagement.PersonalizedContent;
+using Orckestra.Tools.ConditionalContent.Providers;
+using Orckestra.Tools.ConditionalContent.Types.Config;
+
+namespace ConditionalContent.Tests.Services
+{
+    [TestFixture()]
+    public class ConditionalContentServiceTests
+    {
+        public AutoMocker Container { get; set; }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            Container = new AutoMocker();
+
+            //Container.CreateInstance()
+        }
+
+        public static string DefaultContent = "Default Content";
+        public static string IsGuestContent = "Is Guest Content";
+
+        public string GetQuery =  $@"{{
+  ""groups"": [
+    {{
+      ""content"": ""{IsGuestContent}"",
+      ""query"": {{
+        ""and"": [
+          {{
+            ""=="": [
+              {{
+                ""var"": ""Test1.isGuest""
+              }},
+              true
+            ]
+          }}
+        ]
+      }}
+    }}
+  ],
+  ""defaultContent"": ""{DefaultContent}""
+}}";
+
+
+        [Test()]
+        public void WHEN_Providers_IS_NOT_used_in_query_THEN_SHOULD_get_data()
+        {
+            // Arrange
+            var conditionProvider1 = new Mock<IConditionProvider>();
+            conditionProvider1.Setup(d => d.Name).Returns("Test1");
+            conditionProvider1.Setup(d => d.GetData()).Returns(new { isGuest = true});
+            var conditionProvider2 = new Mock<IConditionProvider>();
+            conditionProvider2.Setup(d => d.Name).Returns("Test2");
+            conditionProvider2.Setup(d => d.GetData()).Returns(new { isGuest = false });
+
+            var query = GetQuery;
+
+            var sut = new ConditionalContentService(new List<IConditionProvider>() { conditionProvider1.Object, conditionProvider2.Object});
+
+            // Act
+            var result = sut.GetContent(query);
+
+            // Assert
+
+            result.Should().Be(IsGuestContent);
+            conditionProvider1.Verify(d => d.GetData(), Times.Once(), "Used provider should be called");
+            conditionProvider2.Verify(d => d.GetData(), Times.Never(), "Unused provider should not called");
+        }
+    }
+}
--- ConditionalContent/Services/ConditionalContentService.cs
+++ ConditionalContent/Services/ConditionalContentService.cs
@@ -11,16 +11,23 @@ namespace Orckestra.ExperienceManagement.PersonalizedContent
 {
     public class ConditionalContentService : IConditionalContentService
     {
+        private IEnumerable<IConditionProvider> ConditionProviders { get; set; }
+
+        public ConditionalContentService(IEnumerable<IConditionProvider> conditionProviders)
+        {
+            ConditionProviders = conditionProviders ?? throw new ArgumentNullException(nameof(conditionProviders));
+        }
+
         public string GetContent(string parameters)
         {
             var parameterJObject = JObject.Parse(parameters);
             var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
 
-            //TODO: call Lazy only on ask
-            var conditionProviders = ServiceLocator.GetServices<IConditionProvider>();
+            var usedProviders = GetUsedProviders(parameterJObject["groups"]);
 
-
-            var dataToApply = conditionProviders.ToDictionary(d => d.Name, d => d.GetData());
+            var dataToApply = ConditionProviders
+                .Where(provider => usedProviders.Contains(provider.Name))
+                .ToDictionary(provider => provider.Name, d => d.GetData());
 
             foreach (var group in parameterJObject["groups"])
             {
@@ -32,6 +39,37 @@ namespace Orckestra.ExperienceManagement.PersonalizedContent
             }
 
             return (string)parameterJObject["defaultContent"];
+        }
+
+        private HashSet<string> GetUsedProviders(JToken data)
+        {
+            var providers = new HashSet<string>();
+
+            void CrawlProviders(JToken j)
+            {
+                if (j is JProperty property && property.Name == "var")
+                {
+                    try
+                    {
+                        var providerName = ((JValue)property.Value).Value.ToString().Split('.').First();
+                        providers.Add(providerName);
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+                else
+                {
+                    foreach (var child in j.Children())
+                    {
+                        CrawlProviders(child);
+                    }
+                }
+            }
+
+            CrawlProviders(data);
+            return providers;
         }
     }
 }
