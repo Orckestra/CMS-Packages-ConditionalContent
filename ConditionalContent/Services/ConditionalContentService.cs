6e257e6e13a4378efe8c0fb1a159e5f45d69cda9
Olga Ignatenko <Olga.Ignatenko@orckestra.com>
2022-10-03 13:22:34 +0000

    Merged PR 10798: fixed issue with parsing parameter date
    fixed issue with parsing parameter date
    
    Related work items: #65683

--- ConditionalContent/Services/ConditionalContentService.cs
+++ ConditionalContent/Services/ConditionalContentService.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Composite.Core;
-using JsonLogic.Net;
+﻿using JsonLogic.Net;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Orckestra.Tools.ConditionalContent.Providers;
 using Orckestra.Tools.ConditionalContent.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Orckestra.ExperienceManagement.PersonalizedContent
 {
@@ -20,14 +20,16 @@ namespace Orckestra.ExperienceManagement.PersonalizedContent
 
         public string GetContent(string parameters)
         {
-            var parameterJObject = JObject.Parse(parameters);
+            var parameterJObject = JToken.Parse(parameters);
+            ChangeDateToIntNodeValues(parameterJObject);
+
             var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
 
             var usedProviders = GetUsedProviders(parameterJObject["groups"]);
 
             var dataToApply = ConditionProviders
                 .Where(provider => usedProviders.Contains(provider.Name))
-                .ToDictionary(provider => provider.Name, d => d.GetData());
+                .ToDictionary(provider => provider.Name, d => ChangeDateToIntObjectValues(d.GetData()));
 
             foreach (var group in parameterJObject["groups"])
             {
@@ -39,6 +41,40 @@ namespace Orckestra.ExperienceManagement.PersonalizedContent
             }
 
             return (string)parameterJObject["defaultContent"];
+        }
+        
+        private object ChangeDateToIntObjectValues(object data)
+        {
+            var node = JToken.Parse(JsonConvert.SerializeObject(data));
+            ChangeDateToIntNodeValues(node);
+            return node.ToObject(typeof(object));
+        }
+        
+        private void ChangeDateToIntNodeValues(JToken node)
+        {
+            if (node.Type == JTokenType.Object)
+            {
+                foreach (JProperty child in node.Children<JProperty>())
+                {
+                    ChangeDateToIntNodeValues(child.Value);
+                }
+            }
+            else if (node.Type == JTokenType.Date)
+            {
+                var value = node.Value<DateTime>();
+                if (node is JValue prop)
+                {
+                    prop.Value = value.Ticks;
+                    node.Value<long>();
+                }
+            }
+            else if (node.Type == JTokenType.Array)
+            {
+                foreach (var child in node.Children())
+                {
+                    ChangeDateToIntNodeValues(child);
+                }
+            }
         }
 
         private HashSet<string> GetUsedProviders(JToken data)
